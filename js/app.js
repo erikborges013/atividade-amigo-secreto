53caddc9bbd0ab8b8aec93267d985e0764ec7ead
erikborges013 <erikborgesofficial@gmail.com>
2025-04-06 17:20:24 -0300

    upload arquivos

---
+++ js/app.js
@@ -0,0 +1,46 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    } 
+    amigo.value = '';
+    console.log(amigos);
+}
+
+function sortear() {
+    embaralhar(amigos);
+
+let sorteio = document.getElementById('lista-sorteio');
+
+    for (i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '--->' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '--->' + amigos[i + 1] + '<br>';
+
+        }
+    }
+
+}
+//Essa parte abaixo foi a copiada do professor. Me explique detalhadamente.
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    document.getElementById('lista-amigos').textContent = '';
+    document.getElementById('lista-sorteio').textContent = '';
+    amigos = [];
+
+}
